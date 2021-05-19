b27497c0fda7ecc71391ee829a98697e7b666b46
Isaac Rocha <isaac.ultrazero@gmail.com>
2021-05-19 17:14:53 -0300

    uploaded

---
+++ README.md
@@ -0,0 +1,2 @@
+# [rochatisaac.github.io/when-is-my-bday](https://rochatisaac.github.io/when-is-my-bday/)
+How many days left for your bDay?
---
+++ engine.js
@@ -0,0 +1,44 @@
+function whenIsMybDay(){
+
+                    var $day = document.querySelector("#day");
+                    var $month = document.querySelector("#month");
+                    var $result = document.querySelector("#result");
+                    var $body = document.querySelector("body");
+                    var test = $day.value+$month.value;
+                   
+                    
+                    if(!isNaN($day.value) && !isNaN($month.value) && !isNaN(test)){
+
+                      var dataAtual = new Date();
+                      dataAtual.setHours(0);
+                      dataAtual.setMinutes(0);
+                      dataAtual.setSeconds(0);
+                      dataAtual.setMilliseconds(0);
+                      var anoAtual = dataAtual.getFullYear();
+                      var dataNiver = new Date(anoAtual,parseInt($month.value)-1,parseInt($day.value));
+                      
+                      var dataAtualTS = dataAtual.getTime();
+                      var dataNiverTS = dataNiver.getTime();
+                      
+                      if(dataNiverTS<dataAtualTS){
+                          dataNiver.setFullYear(++anoAtual);
+                          dataNiverTS = dataNiver.getTime();
+                      }
+                      if(dataNiverTS===dataAtualTS){
+                          $result.innerHTML ="🎂️ Feliz Aniversário! 🎉️🎉️🎉️";
+                          $result.style.fontSize= "100px";
+                          $body.style.animation= "transition 3s infinite"
+                          return;
+                      }
+
+                      const UM_DIA_TS = 24*60*60*1000 //quantos milisegundos em um dia
+                      
+                          $result.innerHTML = "Faltam " + (dataNiverTS - dataAtualTS)/UM_DIA_TS + " dias para o seu aniversário!";
+                          $result.style.fontSize= "30px";
+                      
+                    } else{
+                      alert("Dia ou mês inválido");
+                    }
+
+              
+              };               
